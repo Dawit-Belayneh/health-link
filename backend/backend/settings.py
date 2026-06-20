fa711253e63b20dda8de079721a17068a821ed86
Dawit-Belayneh <dawitbelayneh9@gmail.com>
2026-06-20 15:06:05 +0300

    add apps in setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'hospitals',
+    'doctors',
+    'medical_records',
+    'patients',
 ]
 
 MIDDLEWARE = [
