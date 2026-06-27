1d824a0d4b149db79d32ed41b689b32315ad0e0e
Dawit-Belayneh <dawitbelayneh9@gmail.com>
2026-06-27 22:07:13 +0300

    api view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,17 @@
 from rest_framework import generics
 from hospitals.models import Hospital
-from .serializers import HospitalSerializer
+from patients.models import Patient
+from doctors.models import Doctor
+from medical_records.models import MedicalRecord
+from .serializers import HospitalSerializer, PatientSerializer, DoctorSerializer, MedicalRecordSerializer
+
+class PatientListCreateView(generics.ListCreateAPIView):
+    queryset = Patient.object.all()
+    serializer_class = PatientSerializer
+
+class PatientDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
 
 class HospitalListCreateView(generics.ListCreateAPIView):
     queryset = Hospital.objects.all()
@@ -9,3 +20,19 @@ class HospitalListCreateView(generics.ListCreateAPIView):
 class HospitalDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Hospital.objects.all()
     serializer_class = HospitalSerializer
+
+class DoctorListCreateView(generics.ListCreateAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+
+class DoctorDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+
+class MedicalRecordListCreateView(generics.ListCreateAPIView):
+    queryset = MedicalRecord.objects.all()
+    serializer_class = MedicalRecordSerializer
+
+class MedicalRecordDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MedicalRecord.objects.all()
+    serializer_class = MedicalRecordSerializer
