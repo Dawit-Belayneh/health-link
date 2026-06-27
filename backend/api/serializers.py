626a030b055ef20e63794574e90b401c79bddac7
Dawit-Belayneh <dawitbelayneh9@gmail.com>
2026-06-27 22:00:59 +0300

    serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,26 @@
 from rest_framework import serializers
 from hospitals.models import Hospital
+from doctors.models import Doctor
+from medical_records.models import MedicalRecord
+from patients.models import Patient
 
+
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = "__all__"
+        
 class HospitalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hospital
+        fields = "__all__"
+
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = "__all__"
+
+class MedicalRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MedicalRecord
         fields = "__all__"
