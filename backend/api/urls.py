d19c0487060734a6324a0a94d828352a26b980e9
Dawit-Belayneh <dawitbelayneh9@gmail.com>
2026-06-28 13:59:48 +0300

    urls views

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('hospital/', views.HospitalListCreateView.as_view(), name='hospital-list-create'),
     path('hospitals/<int:pk>/', views.HospitalDetailView.as_view(), name='hospital-detail'),
+    path('patient/', views.PatientListCreateView.as_view(), name='patient-list-create'),
+    path('patients/<int:pk>/', views.PatientDetailView.as_view(), name='patient-detail'),
+    path('doctor/', views.DoctorListCreateView.as_view(), name='doctor-list-create'),
+    path('doctors/<int:pk>/', views.DoctorDetailView.as_view(), name='doctor-detail'),
+    path('medical_record/', views.MedicalRecordListCreateView.as_view(), name='medical-record-list-create'),
+    path('medical_records/<int:pk>/', views.MedicalRecordDetailView.as_view(), name='medical-records-detail'),
 ]
