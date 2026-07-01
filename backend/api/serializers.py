82291ee82525f8694c747083103017e79916f72a
Dawit-Belayneh <dawitbelayneh9@gmail.com>
2026-07-01 21:11:05 +0300

    register serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,7 +3,27 @@ from hospitals.models import Hospital
 from doctors.models import Doctor
 from medical_records.models import MedicalRecord
 from patients.models import Patient
+from users.models import User
 
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "password",
+            "role",
+        )
+
+    def create(self, validated_data):
+        return User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+            role=validated_data["role"],
+        )
 
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
