2bb3d0ec46211bfc5b096c542689e13d7ef18aba
Dawit-Belayneh <dawitbelayneh9@gmail.com>
2026-07-03 10:58:36 +0300

    update model

--- backend/patients/models.py
+++ backend/patients/models.py
@@ -5,4 +5,4 @@ class Patient(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='patient_profile')
     blood_type = models.CharField(max_length=3, choices=[('A+', 'A+'), ('A-', 'A-'), ('B+', 'B+'), ('B-', 'B-'), ('AB+', 'AB+'), ('AB-', 'AB-'), ('O+', 'O+'), ('O-', 'O-')])
     allergies = models.TextField(blank=True)
-    emeregency_contact = models.CharField(max_length=100, blank=True)
+    emergency_contact = models.CharField(max_length=100, blank=True )
