bc8b00ab48b259741abfc718c51c19052a998958
Dawit-Belayneh <dawitbelayneh9@gmail.com>
2026-07-07 14:53:42 +0300

    add permission

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,33 +4,44 @@ from patients.models import Patient
 from doctors.models import Doctor
 from medical_records.models import MedicalRecord
 from .serializers import HospitalSerializer, PatientSerializer, DoctorSerializer, MedicalRecordSerializer, RegisterSerializer
+from .permissions import IsPatient, IsHospitalStaff, IsAdmin
+from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
 class PatientListCreateView(generics.ListCreateAPIView):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+    permission_classes = [IsAuthenticated, IsPatient]
 
 class PatientDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+    permission_classes = [IsAuthenticated, IsPatient]
+
+
+    def get_object(self):
+        return self.request.user.patient_profile
 
 class HospitalListCreateView(generics.ListCreateAPIView):
     queryset = Hospital.objects.all()
     serializer_class = HospitalSerializer
+    permission_classes = [IsAuthenticated, IsAdmin]
 
 class HospitalDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Hospital.objects.all()
     serializer_class = HospitalSerializer
+    permission_classes = [IsAuthenticated, IsAdmin]
 
 class DoctorListCreateView(generics.ListCreateAPIView):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
+    permission_classes = [IsAuthenticated, IsHospitalStaff]
 
 class DoctorDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
-
+    permission_classes = [IsAuthenticated, IsHospitalStaff]
 class MedicalRecordListCreateView(generics.ListCreateAPIView):
     queryset = MedicalRecord.objects.all()
     serializer_class = MedicalRecordSerializer
